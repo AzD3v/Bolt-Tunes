f7157b752e5501e0c2ca9c3b93ac4e5403f423a0
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-04-22 18:54:27 +0000

    Menu lateral pronto (animação incluída)

---
+++ js/homepage.js
@@ -0,0 +1,15 @@
+/* HOMEPAGE JAVASCRIPT */
+
+$(document).ready(function(){
+
+		$(".ion-close-circled").click(function(){
+					$(".sidebar-menu").addClass("hide-menu");
+					$(".toogle-menu").addClass("opacity-one");
+		});
+
+		$(".toogle-menu").click(function(){
+					$(".sidebar-menu").removeClass("hide-menu");
+					$(".toogle-menu").removeClass("opacity-one");
+		});
+
+});
