960a4b39e6093ec6da65b0f976c1d7daf5219363
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-05-01 11:20:43 +0000

    Área central da página melhorada

--- js/homepage.js
+++ js/homepage.js
@@ -1,14 +1,15 @@
 /* HOMEPAGE JAVASCRIPT */
-$(document).ready(function () {
 
-	$(".ion-close-circled").click(function () {
-		$(".sidebar-menu").addClass("hide-menu");
-		$(".toogle-menu").addClass("opacity-one");
-	});
+$(document).ready(function(){
 
-	$(".toogle-menu").click(function () {
-		$(".sidebar-menu").removeClass("hide-menu");
-		$(".toogle-menu").removeClass("opacity-one");
-	});
+		$(".ion-close-circled").click(function(){
+					$(".sidebar-menu").addClass("hide-menu");
+					$(".toogle-menu").addClass("opacity-one");
+		});
+
+		$(".toogle-menu").click(function(){
+					$(".sidebar-menu").removeClass("hide-menu");
+					$(".toogle-menu").removeClass("opacity-one");
+		});
 
 });
