9465f9cc8af05d50e2ebb7f6d0cfce62f7ef9f4f
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-05-05 21:41:40 +0000

    Secção que irá conter os favoritos adicionada (estaticamente apenas), pequenas correções gerais para melhorar legibilidade do código introduzidas

--- js/homepage.js
+++ js/homepage.js
@@ -1,19 +1,38 @@
-/* HOMEPAGE JAVASCRIPT */
+/* ******************* */ 
+/* HOMEPAGE JAVASCRIPT */ 
+/* ******************* */ 
 
 $(document).ready(function(){
 
+		// Esconder por predefinição as diversas áreas de conteúdo do catálogo
 		$("#navbarResponsive").hide();
 		$("#resultados-pesquisa").hide();
+		$("#musicas-favoritas").hide();
 
-		/* "Toogler" do conteúdo inicial do website */
+		// "Toogler" da lista de playlists do utilizador (homepage)
+		$(".lista-playlists").click(function(){
+			$("#musicas-favoritas").fadeOut();
+			$("#resultados-pesquisa").fadeOut();
+			$("#homepage-principal").fadeIn();
+		});
+
+		// "Toogler" dos resultados de pesquisa que o utilizador efetuar
 		$(".pesquisa-personalizada").click(function(){
 			$("#homepage-principal").fadeOut();
 			$(".pesquisa").hide();
+			$("#musicas-favoritas").hide();
 			$("#navbarResponsive").fadeIn();
 			$("#resultados-pesquisa").fadeIn();
 		});
 
-		/* Toogler do menu lateral */
+		// "Toogler" da secção de músicas favoritas do utilizador
+		$(".lista-favoritos").click(function() {
+			$("#homepage-principal").fadeOut();
+			$("#resultados-pesquisa").fadeOut();
+			$("#musicas-favoritas").fadeIn();
+		});
+
+		// "Toogler" do menu lateral
 		$(".ion-close-circled").click(function(){
 					$(".sidebar-menu").addClass("hide-menu");
 					$(".toogle-menu").addClass("opacity-one");
@@ -40,12 +59,11 @@ $(document).ready(function(){
 		/* Efeito fadeIn e fadeOut do footer */
 		$(document).scroll(function() {
 				var y = $(this).scrollTop();
-				if(y > 500) {
+				if(y > 520) {
 						$('footer').fadeIn();
 				} else {
 						$('footer').fadeOut();
 				}
 		});
-
 
 });
