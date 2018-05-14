0c17b00862653ee8ec598be26a4b78d02838a69f
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-05-14 14:58:22 +0000

    Funcionalidade que apresenta constantemente os próximos 10 resultados de pesquisa a funcionar corretamente

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -43,7 +43,9 @@ $(document).ready(function(){
     // Pedido HTTP GET ao serviço YouTube
     $.get(url, function(response, status){
      if (status == 'success') {
-       for (let resultado of response.items) {
+      nextPageToken = response.nextPageToken;
+      console.log(nextPageToken); 
+      for (let resultado of response.items) {
 
         let video = $("<div class='wrapper'><div class='resposta'><iframe src='https://www.youtube.com/embed/"+resultado.id.videoId+"'></iframe></div><div class='resposta_nome'>"+resultado.snippet.title+"</div><div class='container_button'><div id='sub_container_button'><button class='btn botao-opcao0'>Ouvir faixa</button><button class='btn botao-opcao1'>Adicionar faixa</button><button class='btn botao-opcao2'>Saber mais +</button></div></div></div>");
 
@@ -77,58 +79,7 @@ $(document).ready(function(){
             $(".resposta_favoritos").append("<br>").append(video2);
               console.log(getElementsByClasseName("wrapper", 1));
             
-            }); 
-
-            /* ÁREA DE TRABALHO DO CARINA */  
-
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-
-
-            
-          
-            /* ÁREA DE TRABALHO DO PAULO */          
+            });
             
             // Botão que permitirá saber mais informações acerca da faixa, álbum e artista 
           $(".botao-opcao2").click(function(){
@@ -188,73 +139,14 @@ $(document).ready(function(){
 
 
           });
-        
-       
-       
-       
-       
-       
-       
+  
         }
      }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     /* ÁREA DE TRABALHO DO BOTÃO 3 (CARINA) */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
      //console.log('status, response');
     
     });
-  };
-
-  $(url, function(response, status) {
-      var nextPageToken = response.nextPageToken;
-  });
-    
+    var nextPageToken;
     $("#botao-mais").click(function(){
       
       var next10 = "https://www.googleapis.com/youtube/v3/search?q="
@@ -267,31 +159,34 @@ $(document).ready(function(){
     + "&key=" + youtubeAPIKey;
       
     $.get(next10, function(response, status){
+      nextPageToken = response.nextPageToken;
         if (status == 'success') {
           for (let resultado of response.items) {
-   
+            
            let video = $("<div class='wrapper'><div class='resposta'><iframe src='https://www.youtube.com/embed/"+resultado.id.videoId+"'></iframe></div><div class='resposta_nome'>"+resultado.snippet.title+"</div><div class='container_button'><div id='sub_container_button'><button class='btn botao-opcao1'>Adicionar faixa</button><button class='btn botao-opcao2'>Saber mais +</button></div></div></div>");
-   
-          // let nome = $("<div class='nome'></div>").text(resultado.snippet.title);
+           
+           // let nome = $("<div class='nome'></div>").text(resultado.snippet.title);
          
-              video.click(function(){         
-                window.open("https://www.youtube.com/embed/"+resultado.id.videoId);
-                console.log(resultado);
-              });
-              
-              $("#respostas").append("<br>").append(video);
-              
-             // $("#resposta_nome").append("<br>").append(nome);
+           video.click(function(){         
+             window.open("https://www.youtube.com/embed/"+resultado.id.videoId);
+             console.log(resultado);
+            });
+            
+            $("#respostas").append("<br>").append(video);
+            
+            // $("#resposta_nome").append("<br>").append(nome);
           }
         }
         //console.log('status, response');
-            
+        
       });    
     
     });
   
   }
+
+});
   
-)});
+});
 
 });
